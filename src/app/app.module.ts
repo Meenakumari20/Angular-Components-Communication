b49c7fcd8d3d09684058cf2cd43b3e4e9baa8a7e
Sharad Shinde <sharad.shinde@coditas.com>
2018-06-06 09:15:44 +0530

    code init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ParentToChildComponent } from './parent-to-child/parent-to-child.component';
+
+const routes: Routes = [
+  { path: 'parent-to-child', component: ParentToChildComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentToChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
