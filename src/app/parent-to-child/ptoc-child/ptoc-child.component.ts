dfc256628e91c33a1df3d03b67127b77607916ec
Sharad Shinde <sharad.shinde@coditas.com>
2018-06-06 09:42:01 +0530

    Parent To Child Communication Complete

--- src/app/parent-to-child/ptoc-child/ptoc-child.component.ts
+++ src/app/parent-to-child/ptoc-child/ptoc-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ptoc-child',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ptoc-child.component.css']
 })
 export class PtocChildComponent implements OnInit {
+
+  @Input() parentInput = '';
 
   constructor() { }
 
